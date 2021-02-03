52551395bb6e0f0e7dae8265b1f7ea4bceb6d2db
Carlos Urquiza <webquiza@gmail.com>
2021-02-02 20:01:03 -0800

    add final code for lesson
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   $("form#transportation_survey").submit(function(event) {
     event.preventDefault();
+    
     $("#work-responses").show();
     $("input:checkbox[name=work-transportation]:checked").each(function() {
       const workTransportationMode = $(this).val();
       $("#work-responses").append(workTransportationMode + "<br>");
     });
+
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function() {
+      const funTransportationMode = $(this).val();
+      $("#fun-responses").append(funTransportationMode + "<br>");
+    });
+
     $("#transportation_survey").hide();
   });
 });
